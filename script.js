1a8a136635ea59f75335bbdd0f67c999628ac037
JuanGualim <gua24852@uvg.edu.gt>
2026-04-23 18:10:04 -0600

    Función para obtener csv con series

--- script.js
+++ script.js
@@ -133,4 +133,24 @@ async function deleteSeries(id) {
     loadSeries()
 }
 
+async function exportCSV() {
+    const res = await fetch(`${API}/series`)
+    const data = await res.json()
+
+    let csv = "ID,Name,Current Episode,Total Episodes\n"
+
+    data.forEach(series => {
+        csv += `${series.id},${series.name},${series.current_episode},${series.total_episodes}\n`
+    })
+
+    const blob = new Blob([csv], { type: "text/csv" })
+    const url = window.URL.createObjectURL(blob)
+
+    const a = document.createElement("a")
+    a.href = url
+    a.download = "series.csv"
+    a.click()
+
+    window.URL.revokeObjectURL(url)
+}
 window.onload = loadSeries
